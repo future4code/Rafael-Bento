4a32d3417ca21d40550218e8a051e994a2701673
rafaelbentoti <rafaelbento.ti@gmail.com>
2021-06-17 16:17:31 -0300

    17062021

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,74 @@
+// Exercícios de interpretação de código
+// Exercício 01
+
+//  function minhaFuncao(variavel) {
+//  	return variavel * 5
+//  }
+
+// console.log(minhaFuncao(2)) // 10
+// console.log(minhaFuncao(10)) // 50
+
+// RESPOSTA a) Sera impresso 10 , 50.
+// RESPOSTA b) ACREDITO QUE NÃO IRIA MOSTRAR O RESULTADO.
+
+// Exercício 01 - FIM
+
+// Exercício 02
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+// RESPOSTA a) Essa funcão trás se o resultado é true ou false, caso escreva o texto sendo dirente de "cenoura".
+// RESPOSTA b) i. true - ii. true - ii. true
+
+// Exercício 02 - FIM
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// Exercícios de escrita de código
+
+// function meusDados() {
+//     console.log("Eu sou Rafael, tenho 35 anos, moro em Brasília e sou CORAJOSO nessa jornada.")
+// }
+
+// meusDados()
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// const meuNome = prompt("Digite o seu nome: ")
+// const minhaIdade = number(prompt("Digite a idade: "))
+// const meuEndereco = prompt("Digite o endereço: ")
+// const minhaProfissao = prompt("Digite a profissão: ")
+
+// function meusDados2(nome, idade, endereco, profissao) {
+//     const textos = `Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e sou ${profissao}`
+//     return textos
+// }
+
+// const resultado = meusDados2(meuNome, minhaIdade, meuEndereco, minhaProfissao)
+
+// console.log(resultado)
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// EXERCÍCIO 02 - INÍCIO
+
+// function  doisNumeros(numero1, numero2) {
+//     return numero1 >= numero2
+// }
+
+// const resultado = doisNumeros(5, 7)
+// console.log(resultado)
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
